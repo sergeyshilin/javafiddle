ad985a5274ef82e8f2240951c30d2f9936d234eb
Сергей Шилин <snape@liceum8.ru>
2013-10-25 00:14:53 +0400

    updated merging

---
+++ JavaFiddle-war/src/java/com/javafiddle/web/beans/CodeTextBean.java
@@ -0,0 +1,45 @@
+package com.javafiddle.web.beans;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author snape
+ */
+@ManagedBean(name="code")
+@RequestScoped
+public class CodeTextBean implements Serializable {
+    
+    private String text = "";
+    private String file = "Example.txt";
+    
+    public String getText() {
+        return text;
+    }
+    
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @PostConstruct
+    public void readFromFile() {
+        try {
+            BufferedReader br = new BufferedReader( 
+                new InputStreamReader(
+                    CodeTextBean.this.getClass().getResourceAsStream(file)));
+            String strLine;
+            while ((strLine = br.readLine()) != null)   {
+               text += strLine + "\n";
+            }
+        } catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+        }
+        
+    }
+       
+}
