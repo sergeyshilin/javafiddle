05a93bf944b8112c938e7529854965c6a5b578ee
Сергей Шилин <snape@liceum8.ru>
2013-10-30 16:55:55 +0400

    Project tree saver interface added. Vikont should implement this one.

---
+++ JavaFiddle-war/src/java/com/javafiddle/web/projecttree/ProjectNode.java
@@ -0,0 +1,16 @@
+package com.javafiddle.web.projecttree;
+
+public class ProjectNode {
+    private String path = null;
+    private Enum type;
+    private String name;
+    private ProjectNode parent;
+    private String content;
+    
+    public String getPath() {
+        return path;
+    }
+    
+    
+    
+}
---
+++ JavaFiddle-war/src/java/com/javafiddle/web/projecttree/ProjectTree.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javafiddle.web.projecttree;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author snape
+ */
+public class ProjectTree extends ArrayList<ProjectNode> {
+    
+}
